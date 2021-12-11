099fa0f41e8afd792149650e3ea790119e3eadaa
Senad Topalović <stopalovic1@etf.unsa.ba>
2021-12-11 18:39:34 +0100

    Testiranje azure-a

--- RMBCloneAPI/Controllers/WeatherForecastController.cs
+++ RMBCloneAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace RMBCloneAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Hladno"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
