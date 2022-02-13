024cdc4a76ec390a596b92b7ba51bbf4ce6e5fb2
Senad Topalović <stopalovic1@etf.unsa.ba>
2022-02-13 18:26:59 +0100

    fixed conflicts from branch feature/atmfilter

--- RmbClone.Library/DataAccess/IBranchData.cs
+++ RmbClone.Library/DataAccess/IBranchData.cs
@@ -12,8 +12,6 @@ namespace RmbClone.Library.DataAccess
         Task<List<BranchResponseModel>> GetAllBranchesAsync();
         Task<BranchDBModel> GetBranchByIdAsync(string id);
         Task InsertBranchAsync(BranchRequestModel model);
-        Task<BranchDBModel> GetBranchByIdAsync(string id);
-        Task DeleteBranchAsync(string id);
         Task UpdateBranchAsync(string id, BranchRequestModel model);
     }
 }
