2b4c4c657abbacb98958e4324ea5d2bd74c7c3ca
Senad Topalović <stopalovic1@etf.unsa.ba>
2022-02-16 02:16:39 +0100

    fixed typo in stored procedure for filtering

--- RmbCloneDb/dbo/Stored Procedures/spBranch_Filter.sql
+++ RmbCloneDb/dbo/Stored Procedures/spBranch_Filter.sql
@@ -7,10 +7,10 @@ begin
 	set nocount on;
 	select  br.*
 	from [dbo].[Branch] as br
-	inner join [dbo].[BranchServiceType] as brst on (br.BranchServiceTypeId=brst.Id)
-	inner join [dbo].[BranchType] as brt on (br.BranchTypeId=brt.Id)
-	inner join [dbo].[City] as c on (br.CItyId=c.Id)
+	inner join [dbo].[BranchServiceType] as brst on (br.BranchServiceTypeId = brst.Id)
+	inner join [dbo].[BranchType] as brt on (br.BranchTypeId = brt.Id)
+	inner join [dbo].[City] as c on (br.CityId = c.Id)
 	where c.[Name] = IIF(@City is null, c.[Name], @City) 
-	and brt.Name=IIF(@BranchType is null, brt.[Name] ,@BranchType) 
-	and brst.[name]=IIF(@BranchServiceType is null,brst.[Name],@BranchServiceType);
+	and brt.Name = IIF(@BranchType is null, brt.[Name] ,@BranchType) 
+	and brst.[name] = IIF(@BranchServiceType is null, brst.[Name], @BranchServiceType);
 end
